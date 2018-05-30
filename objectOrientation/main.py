381f0859fe9ccb8d358c00edf947946e94029da9
Sabrina B <sabrinabgbc@email.com>
2018-05-30 13:20:04 -0300

    Update main.py

--- objectOrientation/main.py
+++ objectOrientation/main.py
@@ -1,20 +1,19 @@
 # -*- coding: utf-8 -*-
 __author__ = 'Sabrina'
 
-#instanciar a classe: criar
-#classe: definine o tipo de um objeto e suas características gerais
-#métodos: são ações dos objetos.
+#instantiate class: create object
+#class: define the type of an object and their generics characteristics
+#methods: the actions of objects
 
 class unicorn:
     def __init__(self, color, age, name, subjects): #parâmetros da classe
-    #método inicial para ele mesmo (self), semelhante ao this do Java, é o método construtor de objetos.
+    #construct method
     self.color = color
     self.age = age
     self.name = name
     self.subjects = subjects
 
     def addSubject(self, newSubject):
-        #método abastecer
         self.subjects += newSubject
 
 
