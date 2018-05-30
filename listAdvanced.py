cd061cb9e8fd43dd7b783a7127ab91fefcb0f511
Sabrina B <sabrinabgbc@email.com>
2018-05-30 12:10:10 -0300

    Update listAdvanced.py

--- listAdvanced.py
+++ listAdvanced.py
@@ -2,16 +2,16 @@ __author__ = 'Sabrina'
 # -*- coding: utf-8 -*-
 
 searchCount = 0
-print("\n Tupla")
-tupla = ("Sabrina", "Alexander")
-print(tupla)
+print("\n Tuple")
+firstTuple = ("Sabrina", "Alexander")
+print(firstTuple)
 
 print("\n group - table hash")
 group = {'Alexander', 'Sabrina'}
 search = input("Enter the name to be searched for")
 if search in group:
 	searchCount = searchCount + 1
-	print("The name ", search, "has found for", searchCount, "times")
+	print("The name ", search, "has founded for ", searchCount, " times")
 else:
-	print("The name ", search, "has not found")
+	print("The name ", search, "has not founded.")
 
