3d4cd98b4254e144173688cee29d7f25111a1d46
Sabrina B <sabrinabgbc@email.com>
2018-05-30 12:10:47 -0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -2,7 +2,7 @@ __author__ = 'Sabrina'
 # -*- coding: utf-8 -*-
 
 lista = ['Axel', 'Liv', 'Kraus', 'Stefan']
-name = input("Enter the name to be searched for:")
+name = input("Enter with the name to search:")
 
 #construção da função
 def printMessage():
