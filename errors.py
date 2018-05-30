5938d1188e02c46459da11286a2151308f43ba16
Sabrina B <sabrinabgbc@email.com>
2018-05-30 12:14:25 -0300

    Update errors.py

--- errors.py
+++ errors.py
@@ -1,6 +1,5 @@
 __author__ = 'Sabrina'
 # -*- coding: utf-8 -*-
-
 import time
 
 file = 'text.txt'
@@ -8,9 +7,8 @@ def openFile():
     try:
         open('text.txt')
         return file
-
     except Exception as erro:
-        print("Error in try to open file :", erro)
+        print("Error trying to open file :", erro)
         return False
 
         while not openFile():
@@ -18,20 +16,17 @@ def openFile():
             time.sleep(5)
             print(file, "open")
 
-#exemplo de erro por divisão por 0
-print("Division to 0")
+#example of error: division by 0
+print("Division by 0")
 try:
     resultado = 1 / 0
 
-#resoluções para erros
+#error handling
 except ZeroDivisionError:
     print("Can not divide a number by 0.")
-
 except NameError:
     print("Sorry, you entered some invalid entry, please try again.")
-
 except Exception:
     print("Error!")
-
 except Exception as erro:
     print("Something went wrong: ", erro)
