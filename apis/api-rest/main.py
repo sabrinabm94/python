913b83b24e1146a1662e5a528d8a132a1c133898
sabrinabgbc <sabrinabgbc@email.com>
2018-10-22 21:57:02 -0300

    método post/inset api

--- apis/api-rest/main.py
+++ apis/api-rest/main.py
@@ -1,26 +1,47 @@
 #importar o framework flask
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from client import Client
 from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
-app.config["mongodb://192.168.106.44:27017/DBwaltercoan"]
+app.config["MONGO_URI"] = "mongodb://172.18.0.35:27017/DBsabrina"
 mongo = PyMongo(app)
 
-listClients = [
-    Client(name="Sabrina", email="sabrinabgbc@email.com", phone="5547000000000"),
-    Client(name="Alexander", email="callfrygou@email.com", phone="5547000000000")
-]
+#listClients = [
+#    Client(name="Sabrina", email="sabrinabgbc@email.com", phone="5547000000000"),
+#    Client(name="Alexander", email="callfrygou@email.com", phone="5547000000000")
+#]
 
-@app.route('/api/v1.0/client', methods=['GET'])
+@app.route('/api/v1.0/clients', methods=['GET'])
 def get_tasks():
-    return jsonify({'clients': [umcli.__dict__ for umcli in listClients]})
-#expressão lambda do python
+    clients = []
+    for client in mongo.db.clients.find():
+        newClient = Client()
+        newClient._id = str(client['_id'])
+        newClient.name = str(client['name'])
+        newClient.phone = str(client['phone'])
+        newClient.email = str(client['email'])
+        clients.append(newClient)
+    return jsonify({'clients': [client.__dict__ for client in clients]}), 201
 
-#código principal
+@app.route('/api/v1.0/clients', methods=['POST'])
+def create_client():
+    newcli = Client()
+    newcli._id = ObjectId()
+    newcli.name = request.json['name']
+    newcli.email = request.json['email']
+    newcli.phone = request.json['phone']
+
+    ret = mongo.db.clients.insert_one(newcli.__dict__).inserted_id
+    return jsonify({'id': str(ret)}), 201
+
+
+#config
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(debug=True, host='0.0.0.0', port=8080)
 
 #acesso padrão: http://localhost:5000/api
-#acesso modificado: http://localhost/api
-#acesso versionado: http://localhost/api/v1.0/client
+#acesso modificado: http://localhost:8080/api
+#acesso versionado: http://localhost:8080/api/v1.0/clients
