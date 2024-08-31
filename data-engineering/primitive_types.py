f6a9cdccfa4f0d731a9cc769b35c34bc9e01ee91
Sabrina <sabrinabm94@gmail.com>
2024-08-31 20:57:52 -0300

    constantes e variaveis

---
+++ data-engineering/primitive_types.py
@@ -0,0 +1,37 @@
+#Numbers
+
+#Int
+positive_number = 1
+print("positive_number: ", positive_number)
+
+negative_number = -100
+print("negative_number: ", negative_number)
+
+sum_values = negative_number + positive_number
+print("sum_values: ", sum_values)
+
+#Float
+float_number = 33.3
+print("float_number: ", float_number)
+
+#Boolean
+true_value = True
+print("true_value: ", true_value)
+
+false_value = False
+print("false_value: ", false_value)
+
+#String
+string_value = "Olá, mundo"
+print("string_value: ", string_value)
+
+#String empty
+empty_value = ""
+print("empty_value: ", empty_value)
+
+#Value None
+null_value = None
+print("null_value: ", null_value)
+
+
+    
